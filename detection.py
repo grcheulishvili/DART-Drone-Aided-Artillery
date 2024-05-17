d4f150fd8fd5a7d01e7c348241c20e808b2e00f4
grcheulishvili <88247941+grcheulishvili@users.noreply.github.com>
2024-05-17 22:56:14 +0400

    Changes
    1. detection.py - დაემატა ჰაუბიცას პირობითი კოორდინატი, მანძილი მიზნამდე, დალაგებულია მონაცემები უკეთესად, მართკუთხა კოორდინატები
    2. geo.py - დაემატა აზიმუტის გამოთვლა 
    3. settings.json - შეიცვალა საწყისი წერტილი

--- detection.py
+++ detection.py
@@ -6,61 +6,82 @@ import pprint
 import sys
 import math
 import geo
+import mgrs
+from tabulate import tabulate
 
-SCALE = 50000
-
-# connect to the AirSim simulator
 client = airsim.MultirotorClient()
 client.confirmConnection()
 
-# set camera name and image type to request images and detections
 camera_name = "0"
 image_type = airsim.ImageType.Scene
 
-# set detection radius in [cm]
 client.simSetDetectionFilterRadius(camera_name, image_type, 200 * 100) 
-# add desired object name to detect in wild card/regex format
+# amosacnobi obieqtis saxeli
 client.simAddDetectionFilterMeshName(camera_name, image_type, "Car*") 
     
+mgrs_conv = mgrs.MGRS()
+
+Artillery_coords = (42.164805, 44.078532)
 
 while True:
     rawImage = client.simGetImage(camera_name, image_type)
     if not rawImage:
         continue
     png = cv2.imdecode(airsim.string_to_uint8_array(rawImage), cv2.IMREAD_UNCHANGED)
-    cars = client.simGetDetections(camera_name, image_type)
-    if cars:
-        for car in cars:
-            s = pprint.pformat(car)
+    targets = client.simGetDetections(camera_name, image_type)
+    if targets:
+        for target in targets:
+            s = pprint.pformat(target)
             drone_state = client.getMultirotorState()
 
-            drone_orientation = int(math.degrees(2*math.acos(drone_state.kinematics_estimated.orientation.w_val)))
-            drone_lat = format(client.getGpsData(vehicle_name="my_drone").gnss.geo_point.latitude,'.5f')
-            drone_lon = format(client.getGpsData(vehicle_name="my_drone").gnss.geo_point.longitude, '.5f')
-            distance_to_object = car.relative_pose.position.get_length()
-            
-            object_lat = geo.obj_coordinates(drone_lat, drone_lon, distance_to_object, drone_orientation)[0]
-            object_lon = geo.obj_coordinates(drone_lat, drone_lon, distance_to_object, drone_orientation)[1]
+            # Mewinave Metvaltvalis lat,lon,alt
+            FO_lat = client.getHomeGeoPoint('my_drone').latitude
+            FO_lon = client.getHomeGeoPoint('my_drone').longitude
+            FO_alt = client.getHomeGeoPoint('my_drone').altitude
 
-            print(f"""
-                  
-            \rAfrenis Lat,Lon,Alt: {client.getHomeGeoPoint('my_drone').latitude}, {client.getHomeGeoPoint('my_drone').longitude}, {client.getHomeGeoPoint('my_drone').altitude} m
-            Amjamindeli Lat,Lon: {format(client.getGpsData(vehicle_name="my_drone").gnss.geo_point.latitude,'.5f'), format(client.getGpsData(vehicle_name="my_drone").gnss.geo_point.longitude, '.5f')}
-            Gradusi: {drone_orientation}°
+            # Dronis gadaxra chrdiloetidan-dan, lat, lon, alt, mandzili miznamde 
+            drone_orientation = int(math.degrees(2*math.acos(drone_state.kinematics_estimated.orientation.w_val))) 
+            drone_lat = format(client.getGpsData(vehicle_name="my_drone").gnss.geo_point.latitude,'.6f')
+            drone_lon = format(client.getGpsData(vehicle_name="my_drone").gnss.geo_point.longitude, '.6f')
+            drone_alt = format(client.getGpsData(vehicle_name="my_drone").gnss.geo_point.altitude, '.0f')
+            drone_distance_to_object = int(target.relative_pose.position.get_length())
+            drone_mgrs = mgrs_conv.toMGRS(drone_lat, drone_lon)
+
+            # Miznis lat,lon,alt
+            target_lat = float(format(geo.obj_coordinates(drone_lat, drone_lon, drone_distance_to_object, drone_orientation)[0],'.6f'))
+            target_lon = float(format(geo.obj_coordinates(drone_lat, drone_lon, drone_distance_to_object, drone_orientation)[1],'.6f'))
+            target_alt = format(target.geo_point.altitude, '.0f')
+            target_mgrs = mgrs_conv.toMGRS(target_lat, target_lon)
+
+            distance_to_target = format(geo.distance(Artillery_coords[0], Artillery_coords[1], target_lat, target_lon), '.0f')
+            fwd_azimuth_target = format(geo.bearing(Artillery_coords[0], Artillery_coords[1], target_lat, target_lon), '.0f')
+
+
+            print(f"""         
+    ZOGADI MONACEMEBI
+===============================================================
+    Artillery Lat,Lon : {Artillery_coords[0], Artillery_coords[1]}
+    Artillery MGRS: {mgrs_conv.toMGRS(Artillery_coords[0], Artillery_coords[1])}
             
-            Avtomobili
-            ----------------
-            Manqanis ID: {car.name} 
-            Mandzili obieqtamde:  {car.relative_pose.position.get_length()} m
-            Koordinati lat, lon: {object_lat}, {object_lon} 
-            
+    Dronis Lat,Lon: {format(drone_lat), format(drone_lon)}
+    Dronis MGRS: {drone_mgrs}
+    Dronis gadaxra N-dan: {drone_orientation}°
+    Dronis simagle: {drone_alt} m
+    Mandzili dronidan miznamde: {drone_distance_to_object} m
             """)
 
-            sys.stdout.flush()
-            cv2.rectangle(png,(int(car.box2D.min.x_val),int(car.box2D.min.y_val)),(int(car.box2D.max.x_val),int(car.box2D.max.y_val)),(255,0,0),2)
-            cv2.putText(png, car.name, (int(car.box2D.min.x_val),int(car.box2D.min.y_val - 10)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (36,255,12))
+            print(120 * "_")
+            formatter_result = "{:<15} {:<15} {:<15} {:<15} {:<20} {:<15} {:<15}"
+            print(formatter_result.format(*["Miznis ID", "Azimuti", "Mandzili", "Simagle", "MGRS", "Latitude", "Lontitude"]))
+            results = [target.name, str(fwd_azimuth_target) + '°' , str(distance_to_target) + ' m', str(target_alt) + ' m', target_mgrs, target_lat, target_lon]
+            print(formatter_result.format(*results))
+            print(120 * "_")
 
-    cv2.imshow("AirSim dronis virtualuri chveneba", png)
+            sys.stdout.flush()
+            cv2.rectangle(png,(int(target.box2D.min.x_val),int(target.box2D.min.y_val)),(int(target.box2D.max.x_val),int(target.box2D.max.y_val)),(255,0,0),2)
+            cv2.putText(png, target.name, (int(target.box2D.min.x_val),int(target.box2D.min.y_val - 10)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (36,255,12))
+
+    cv2.imshow("Drone-Aided Artillery(DART): saartilerio dazverva", png)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
     elif cv2.waitKey(1) & 0xFF == ord('c'):
--- geo.py
+++ geo.py
@@ -1,6 +1,9 @@
 import geopy
 from geopy.distance import geodesic
+import geopy.distance
+import pyproj
 
+# obieqtis koordinatebis gamotvla 
 def obj_coordinates(my_lat, my_lon, d, bearing):
     origin = geopy.Point(my_lat, my_lon)
     destination = geodesic(d).destination(origin, bearing)
@@ -8,3 +11,15 @@ def obj_coordinates(my_lat, my_lon, d, bearing):
     lat2, lon2 = destination.latitude, destination.longitude
 
     return lat2, lon2
+
+# mandzilis gamotvla koordinatebs shoris
+def distance(lat1, lon1, lat2, lon2):
+    coords_1 = (lat1, lon1)
+    coords_2 = (lat2, lon2)
+    return geopy.distance.geodesic(coords_1, coords_2).m
+
+# azimutis gasagebad
+def bearing(lon1, lat1, lon2, lat2):
+    geodesic = pyproj.Geod(ellps='WGS84')
+    fwd_azimuth,back_azimuth,distance = geodesic.inv(lon1, lat1, lon2, lat2)
+    return fwd_azimuth
